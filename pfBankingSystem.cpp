a8c8e1635a60231a148f6513f02921f55d790e11
LordEnigma12 <angel.melgar9435@alumnos.udg.mx>
2026-05-13 09:41:26 -0600

    Update pfBankingSystem.cpp
    Estructura base de el menu

--- pfBankingSystem.cpp
+++ pfBankingSystem.cpp
@@ -1,12 +1,63 @@
 #include <iostream>
+#include <string>
+#include <limits>
+
+//funciones
+
+int es_entero();
+
+
+
+//variables globales
+
 
 using namespace std;
 
 int main(){
-	cout << "Hello, World!":
-	cout <<"a melgar le gusta el dih";
-	cout << "Hello, World!";
-	cout<< "cum master";
+	
+	//variables main
+	int opcion_menu;
+	
+	
+	cout << "\n ----------------------- ";
+	cout << "\n    Sistema Bancario     ";
+	cout << "\n ----------------------- ";
+	cout << "\n1: Registro de clientes ";
+	cout << "\n2: Registro de abonos ";
+	cout << "\n3: Listado general de todos los clientes ";	
+	cout << "\n4: Cerrar programa ";
+	cout << "\nIngrese el numero de opcion deseada: ";
+	opcion_menu = es_entero();
+	
+	switch (opcion_menu) {
+		case 1:
+			
+			break;
+		case 2:
+			
+			break;
+		case 3:
+			
+			break;
+		default:
+			cout << "\nSaliendo del programa... ";	
+			break;
+	}
+	
 	
 	return 0;
+}
+
+
+int es_entero() {
+    int valor;
+    while (true) {
+        if (cin >> valor && valor >= 1 && valor <= 4) {
+            return valor;
+        } else {
+            cout << "Error: El numero debe estar entre 1 y 4." << endl;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+    }
 }
